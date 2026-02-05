db8c667eb927b9406f934ddfd54c481b3854dd1f
RyanMac <ryan@ryanmac.com>
2026-02-06 01:26:36 +0800

    fix

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -32,7 +32,7 @@ export const createClient = (config: ClientConfig = {}) => {
     routes,
     fetchOptions:
       process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+        ? { next: { tags: ["prismic"], revalidate: 60 } }
         : { next: { revalidate: 5 } },
     ...config,
   });
