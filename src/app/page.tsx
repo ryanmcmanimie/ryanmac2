340cc81dbd55ea0449d1f2d49013c8a713e229bc
RyanMac <ryan@ryanmac.com>
2026-02-06 01:23:12 +0800

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { SliceZone } from "@prismicio/react";
+import { notFound } from "next/navigation";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 
 export default async function Home() {
   const client = createClient();
-  const page = await client.getSingle("home");
+
+  const page = await client.getSingle("home").catch(() => null);
+
+  if (!page) {
+    notFound();
+  }
 
   return <SliceZone slices={page.data.slices} components={components} />;
 }
